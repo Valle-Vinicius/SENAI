7b631373d2f1bf13eec12f191ecc9a14920cacf3
Valle-Vinicius <vinicius.v.rodrigues6@aluno.senai.br>
2025-03-27 15:42:33 -0300

    RE
    TE

---
+++ teste.py
@@ -0,0 +1,6 @@
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite um número: "))
+num3 = int(input("Digite um número: "))
+media = (num1 + num2 + num3 )/4
+
+print(f"A média dos números totais é {media}")
