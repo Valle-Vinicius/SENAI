7ee0ad584a98a4dd9378305bb8a4047da5515386
Valle-Vinicius <vinicius.v.rodrigues6@aluno.senai.br>
2025-04-01 13:26:22 -0300

    1. Criação de Branch
    Esse é o primeiro Commit realizado durante essa Branch. O commit feito tem de objetivo inserir o arquivo "Arquivo.Py" na Branch.

---
+++ arquivo.py
@@ -0,0 +1,15 @@
+gabarito = ["A", "C", "E", "D", "B", "E", "C"]
+contador = 1
+gabaritoaluno = []
+contadoracertos = 0
+
+for i in range(7):  
+    valor = input(f"Digite a resposta da questão {contador}: ")
+    contador += 1
+    gabaritoaluno.append(valor)
+
+for i in range(7):  
+    if gabaritoaluno[i] == gabarito[i]:
+        contadoracertos += 1
+
+print(f"Ao todo você teve {contadoracertos} acertos.")
